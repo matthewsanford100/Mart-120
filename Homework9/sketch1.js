f515938f5111719599c054a4fee90ce3e850a55d
matthewsanford100 <123434374+matthewsanford100@users.noreply.github.com>
2023-05-12 13:17:37 -0600

    Final Edit

---
+++ Homework9/sketch1.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(220);
+  circle(200,200,300);
+  ellipse(150,200,80,60);
+  ellipse(150,200,20,10);
+  ellipse(275,150,80,60);
+  ellipse(275,150,20,10);
+  triangle(200, 200, 358, 250, 286, 275);
+  ellipse(225,300,140,50);
+  rect(225,275,10,20);
+  rect(245,277,10,20);
+  rect(205,277,10,20);
+  rect(150,350,100,200);
+  line(185, 175, 85, 125);
+  line(215, 125, 300, 100);
+  strokeWeight(3);
+  point(150, 250);
+  point(155, 250);
+  point(160, 250);
+  point(275, 195);
+  point(280, 195);
+  point(285, 195);
+  text('ME', 195, 375);
+  text('by: matthew sanford', 150, 390);
+}
+
